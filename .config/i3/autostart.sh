3b3dac312bb445e244f04ad6cdbbf9e68fe63347
🐢 <tpham2580gmail.com>
2024-12-17 08:51:19 -0800

    added hammer wallpaper and updated autostart.sh

--- .config/i3/autostart.sh
+++ .config/i3/autostart.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 # Set wallpaper using feh
-feh --bg-scale ~/Pictures/mountains-minimalism-red-Moon-1912113-wallhere.com.png
+feh --bg-scale ~/Pictures/hammer.jpg
 
 # Assign workspaces to monitors
-i3-msg "workspace 1; move workspace to output DP-0"
-i3-msg "workspace 11; move workspace to output DP-4"
+i3-msg "workspace 5; move workspace to output DP-2"
+i3-msg "workspace 10; move workspace to output DP-0"
+i3-msg "workspace 20; move workspace to output DP-4"
 
 # Switch to the desired workspaces
-i3-msg "workspace 1"
-i3-msg "workspace 11"
+i3-msg "workspace 5"
+i3-msg "workspace 10"
+i3-msg "workspace 20"
