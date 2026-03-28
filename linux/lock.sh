bcb2b2ebab8a2a183c464e56c6b7dc053f8cf171
🐢 <tpham2580@gmail.com>
2026-03-28 10:38:05 -0700

    updated config with desktop stuff

--- linux/lock.sh
+++ linux/lock.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-set -e 
-i3lock -i ~/Pictures/turtle.png --tiling -u -c 000000
-xset dpms
+set -e
+
+if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
+    swaylock -f -c 000000
+else
+    i3lock -i ~/Pictures/turtle.png --tiling -u -c 000000
+    xset dpms
+fi
